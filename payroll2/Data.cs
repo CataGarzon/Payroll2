44e8333212301477bd21d27ab7d49eb7e7dbbdc3
CataGarzon <95449594+CataGarzon@users.noreply.github.com>
2022-06-22 15:02:05 -0500

    Add files via upload

---
+++ payroll2/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace payroll2
+{
+    public class Data
+    {
+        private double Document;
+        private string FristName;
+        private string LastName;
+        private double Salary;
+        private double WorkedDay;
+        private double transport;
+        private double TotalIcome;
+        private double healthe;
+        private double pension;
+
+        public Data() { }
+
+
+        public double Document1 { get => Document; set => Document = value; }
+        public string FristName1 { get => FristName; set => FristName = value; }
+        public string LastName1 { get => LastName; set => LastName = value; }
+        public double Salary1 { get => Salary; set => Salary = value; }
+        public double WorkedDay1 { get => WorkedDay; set => WorkedDay = value; }
+        public double Transport { get => transport; set => transport = value; }
+        public double TotalIcome1 { get => TotalIcome; set => TotalIcome = value; }
+        public double Healthe { get => healthe; set => healthe = value; }
+        public double Pension { get => pension; set => pension = value; }
+    }
+}
+
+
---
+++ payroll2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace payroll2
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            Data data = new Data();
+
+            try
+            {
+                
+                Console.WriteLine("Welcom the payroll");
+                Console.WriteLine("Insert document:");
+                data.Document1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Insert first name: ");
+                data.FristName1 = Console.ReadLine();
+                Console.WriteLine("Insert last name: ");
+                data.LastName1 = Console.ReadLine();
+                Console.WriteLine("Insert salary :");
+                data.Salary1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Insert Worked Days");
+                data.WorkedDay1 = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Dato invalido");
+                Console.ReadKey();
+            }
+
+            
+
+            data.TotalIcome1 = data.Salary1 * data.WorkedDay1 / 30;
+            data.Healthe = data.TotalIcome1 * 0.04;
+            data.Pension = data.TotalIcome1 * 0.04;
+            data.Transport = 117_172 / 30;
+            data.Transport = data.Transport * data.WorkedDay1;
+            if (data.Salary1 <= 2000000)
+            {
+                data.TotalIcome1 = data.TotalIcome1 + data.Transport;
+            }
+            else
+            {
+                data.TotalIcome1 = data.TotalIcome1;
+            }
+
+
+            Console.WriteLine("Document :" + data.Document1);
+            Console.WriteLine("frist name :" + data.FristName1);
+            Console.WriteLine("last name :" + data.LastName1);
+            Console.WriteLine("salary :" + data.Salary1);
+            Console.WriteLine("worked days :" + data.WorkedDay1);
+            if (data.Salary1 <= 2000000)
+            {
+                Console.WriteLine($"The Transport subsidy is :" + data.Transport);
+            }
+            Console.WriteLine("pension :" + data.Pension);
+            Console.WriteLine("Healthe :" + data.Healthe);
+            Console.WriteLine("Total icomes :" + data.TotalIcome1);
+
+
+
+            TextWriter File = new StreamWriter("C:\\Payroll.txt");
+
+            File.WriteLine($"\nNew Employee");
+            File.WriteLine($"Document: " + data.Document1);
+            File.WriteLine($"First Name: {data.FristName1}");
+            File.WriteLine($"Last Name: {data.LastName1}");
+            File.WriteLine("The salary is: " + data.Salary1);
+            File.WriteLine($"Worked days: {data.WorkedDay1}");
+            if (data.Salary1 <= 2000000)
+            {
+                File.WriteLine($"The Transport subsidy is : {data.Transport}");
+            }
+            File.WriteLine("The Total Accrued is:  " + data.TotalIcome1);
+            File.WriteLine("The Health discount is: " + data.Healthe);
+            File.WriteLine("The Pension discount is: " + data.Pension);
+
+
+            File.Close();
+        }
+
+
+    }
+}
